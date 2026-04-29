96ae45aeeba8be703a6131d58fefe10c20b07508
jossgarcia218-dev <jossgarcia218@gmail.com>
2026-04-29 14:27:09 -0600

    practica

---
+++ UNIDAD 4/PRACTICAS/Practica4/js/controladores/index.controller.js
@@ -0,0 +1,21 @@
+angular.module('app').controller('Index', Index);
+
+Index.$inject = ['$scope', '$log'];
+
+function Index($scope, $log) {
+
+    $scope.formConsultaRuta = {
+        fechaInicio: '',
+        fechaFin: '',
+        idProveedor: '',
+        idAutomovil: '',
+        idDriver: '',
+        idRuta: '',
+    }
+
+    $scope.imprimirMensaje = () => {
+        console.log($scope.formConsultaRuta);
+    }
+
+}
+
