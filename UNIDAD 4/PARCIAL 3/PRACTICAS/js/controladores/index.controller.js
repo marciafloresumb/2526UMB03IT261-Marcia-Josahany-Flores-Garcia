7bb7f858e466edcb0aa6d9e69cc8ff30e3da62ce
jossgarcia218-dev <jossgarcia218@gmail.com>
2026-05-13 15:23:41 -0600

    practica 13052026

---
+++ UNIDAD 4/PARCIAL 3/PRACTICAS/js/controladores/index.controller.js
@@ -0,0 +1,114 @@
+angular.module('app').controller('Index', Index);
+
+Index.$inject = ['$scope', '$log'];
+
+function Index($scope, $log) {
+
+    $scope.optionsProveedores = [
+        {
+            etiqueta: 'UECAC',
+            valor: 1
+        },
+        {
+            etiqueta: 'DHL',
+            valor: 2
+        },
+        {
+            etiqueta: 'ESTAFETA',
+            valor: 3
+        },
+        {
+            etiqueta: 'FedEx',
+            valor: 4
+        }
+    ];
+
+    $scope.optionsAutos = [
+        {
+            etiqueta: 'Chevy',
+            valor: 1
+        },
+        {
+            etiqueta: 'Tornado',
+            valor: 2
+        },
+        {
+            etiqueta: 'Camaro',
+            valor: 3
+        },
+        {
+            etiqueta: 'Suburban',
+            valor: 4
+        }
+    ];
+
+    $scope.optionsDrivers = [
+        {
+            etiqueta: 'Puchi',
+            valor: 1
+        },
+        {
+            etiqueta: 'Omar',
+            valor: 2
+        },
+        {
+            etiqueta: 'Iris',
+            valor: 3
+        },
+        {
+            etiqueta: 'Ariadna',
+            valor: 4
+        }
+    ];
+
+    $scope.formConsultaRuta = {
+        fechaInicio: '',
+        fechaFin: '',
+        idProveedor: '',
+        idAutomovil: '',
+        idDriver: '',
+        idRuta: '',
+    }
+
+    $scope.imprimirMensaje = () => {
+        if(!$scope.formConsultaRuta.fechaInicio){
+            $scope.msgError="ingresa una fecha de inicio";
+            return;
+        }else if(!$scope.formConsultaRuta.fechaFin){
+            $scope.msgError="ingresa una fecha fin";
+            return;
+        }
+
+        console.log($scope.formConsultaRuta);
+    }
+
+    //Función que controla el evento de cambio del input Fecha Inicio
+    $scope.ctrlInpFechaInicio = () => {
+        $scope.msgError="";
+        console.log("Cambiaste una fecha");
+    }
+    //Función que controla el evento de cambio del select proveedor
+    $scope.ctrlSlctProveedor = () => {
+        if (!$scope.formConsultaRuta.idProveedor) {
+            $scope.formConsultaRuta.idAutomovil = '';
+            $scope.formConsultaRuta.idDriver = '';
+            $scope.formConsultaRuta.idRuta = '';
+        }
+        console.log("Seleccionaste otro proveedor");
+    }
+      $scope.ctrlSlctAuto = () => {
+        if (!$scope.formConsultaRuta.idAutomovil) {
+            $scope.formConsultaRuta.idDriver = '';
+            $scope.formConsultaRuta.idRuta = '';
+        }
+        console.log("Seleccionaste otro auto");
+    }
+    $scope.ctrlSlctDriver = () => {
+        if (!$scope.formConsultaRuta.idDriver) {
+            $scope.formConsultaRuta.idRuta = '';
+        }
+        console.log("Seleccionaste otro driver");
+    }
+}
+
+
